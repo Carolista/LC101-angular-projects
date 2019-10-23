162c6e7f31574ed8f56b37502ca3139b0ed2ddd6
Carolista <55961845+Carolista@users.noreply.github.com>
2019-10-22 21:37:11 -0500

    takeoff, land, and abort buttons handled

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -1,15 +1,57 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Exercises: Angular Lesson 3';
 
-  color = 'green';
-  height = 0;
-  width = 0;
-  message = 'Space shuttle ready for takeoff!';
+export class AppComponent {
+    title = 'Exercises: Angular Lesson 3';
+
+    // Top section
+    message = 'Space shuttle ready for takeoff!';
+
+    // Right section
+    shuttleHeight = 0;
+
+    // Graphic field
+    fieldColor = "green";
+    rocketXPos = 0;
+    rocketYPos = 0;
+
+    takeOff(): any {
+        confirm("Confirm that the shuttle is ready for takeoff.")
+        if (true) {
+            this.message = "Shuttle in flight.";
+            this.fieldColor = "blue";
+            this.shuttleHeight = 10000;
+        }
+    }
+
+    land(): any {
+        alert("The shuttle is landing. Landing gear engaged.")
+        if (true) {
+            this.message = "Shuttle has landed.";
+            this.fieldColor = "green";
+            this.shuttleHeight = 0;
+        }
+    }
+
+    abort(): any {
+        confirm("Please confirm that you wish to abort the mission.")
+        if (true) {
+            this.message = "Mission aborted.";
+            this.fieldColor = "red";
+            this.shuttleHeight = 0;
+        }
+    }
+
+    goUp(): any {
+        // look at 30.6.4.2
+    }
+
+    goDown(): any {
+        // look 
+    }
 }
