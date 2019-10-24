7233283b97e7cbaf4344c038baceaa57a9c7fbce
Carolista <55961845+Carolista@users.noreply.github.com>
2019-10-24 11:12:19 -0500

    all tasks including bonus mission completed

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'app-root',
@@ -9,6 +9,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
     title = 'Exercises: Angular Lesson 3';
     takeOffEnabled = true;
+    leftMax = false;
+    rightMax = false;
+    topMax = false;
+    bottomMax = true;
 
     // Top section readout
     message = 'Space shuttle ready for takeoff!';
@@ -17,32 +21,61 @@ export class AppComponent {
     shuttleHeight = 0;
 
     // Graphic field
-    fieldColor = "green";
-    xPos = "0px"; // for moving left and right
+    fieldColor = "#6baa30";
+    flightColor = "#15a5af";
+    edgeColor = "#f18724";
+    landColor = "#6baa30";
+    abortColor = "#c54017";
+
+    xPos = "100px"; // for moving left and right
     yPos = "0px"; // for moving up and down
 
+    constructor(el: ElementRef) {}
+
     moveRocket(shuttleBkg, direction): any {
+        let maxH = parseInt(shuttleBkg.getBoundingClientRect().height);
+        let maxW = parseInt(shuttleBkg.getBoundingClientRect().width);
         if (direction === "right") {
-            // if (parseInt(this.xPos) < shuttleBkg.style.width) {
+            if (parseInt(this.xPos) >= maxW - 80) {
+                this.fieldColor = this.edgeColor;
+                this.rightMax = true;
+            } else {
                 let movement = parseInt(this.xPos) + 10 + 'px';
                 this.xPos = movement;
-            // }
+                this.fieldColor = this.flightColor;
+                this.leftMax = false;
+            }
         } else if (direction === "left") {
-            if (parseInt(this.xPos) >= 10) {
+            if (parseInt(this.xPos) === 0) {
+                this.fieldColor = this.edgeColor;
+                this.leftMax = true;
+            } else {
                 let movement = parseInt(this.xPos) - 10 + 'px';
                 this.xPos = movement;
+                this.fieldColor = this.flightColor;
+                this.rightMax = false;
             }
         } else if (direction === "up") {
-            // if (parseInt(this.yPos) < shuttleBkg.style.height) {
+            if (parseInt(this.yPos) >= maxH - 80) {
+                this.fieldColor = this.edgeColor; 
+                this.topMax = true;
+            } else {
                 let movement = parseInt(this.yPos) + 10 + 'px';
                 this.yPos = movement;
                 this.shuttleHeight = 10000;
-            // }
+                this.fieldColor = this.flightColor;
+                this.bottomMax = false;
+            }
         } else if (direction === "down") {
-            if (parseInt(this.yPos) >= 10) {
+            if (parseInt(this.yPos) === 0) {
+                this.fieldColor = this.landColor;
+                this.bottomMax = true;
+            } else {
                 let movement = parseInt(this.yPos) - 10 + 'px';
                 this.yPos = movement;
                 this.shuttleHeight = 0;
+                this.fieldColor = this.flightColor;
+                this.topMax = false;
             }
         }
     }
@@ -51,7 +84,7 @@ export class AppComponent {
         confirm("Confirm that the shuttle is ready for takeoff.");
         if (true) {
             this.message = "Shuttle in flight.";
-            this.fieldColor = "blue";
+            this.fieldColor = this.flightColor;
             this.shuttleHeight = 10000;
         }
     }
@@ -60,7 +93,7 @@ export class AppComponent {
         alert("The shuttle is landing. Landing gear engaged.");
         if (true) {
             this.message = "Shuttle has landed.";
-            this.fieldColor = "green";
+            this.fieldColor = this.landColor;
             this.shuttleHeight = 0;
             this.yPos = "0px";
         }
@@ -70,7 +103,7 @@ export class AppComponent {
         confirm("Please confirm that you wish to abort the mission.");
         if (true) {
             this.message = "Mission aborted.";
-            this.fieldColor = "red";
+            this.fieldColor = this.abortColor;
             this.shuttleHeight = 0;
             this.yPos = "0px";
         }
